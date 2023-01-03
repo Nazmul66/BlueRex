ae2cedc2326341abeb76cc06b3d063324982bd4e
Nazmul66 <hnazmul748@gmail.com>
2023-01-03 16:41:54 +0600

    add

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// work sections jquery 
+
+$(document).ready(function(){
+
+ $("#web").click(function(){
+
+   $("#img1").show("slow");
+   $("#img2").show("slow");
+   $("#img3").show("slow");
+   $("#img4").hide("slow");
+   $("#img5").hide("slow");
+   $("#img6").hide("slow");
+   $("#img7").hide("slow");
+   $("#img8").hide("slow");
+   $("#img9").hide("slow");
+
+ });
+
+ $("#mobile").click(function(){
+
+   $("#img1").hide("slow");
+   $("#img2").hide("slow");
+   $("#img3").hide("slow");
+   $("#img4").hide("slow");
+   $("#img5").show("slow");
+   $("#img6").hide("slow");
+   $("#img7").show("slow");
+   $("#img8").hide("slow");
+   $("#img9").show("slow");
+
+ });
+
+ $("#app").click(function(){
+
+   $("#img1").show("slow");
+   $("#img2").show("slow");
+   $("#img3").show("slow");
+   $("#img4").show("slow");
+   $("#img5").show("slow");
+   $("#img6").show("slow");
+   $("#img7").show("slow");
+   $("#img8").show("slow");
+   $("#img9").show("slow");
+
+ });
+
+ $("#brand").click(function(){
+
+   $("#img1").hide("slow");
+   $("#img2").hide("slow");
+   $("#img3").hide("slow");
+   $("#img4").show("slow");
+   $("#img5").hide("slow");
+   $("#img6").show("slow");
+   $("#img7").hide("slow");
+   $("#img8").show("slow");
+   $("#img9").hide("slow");
+
+ });
+
+});
